7871b34bd6346d299a998ad51b69efc114ff86f2
Sweta Rani <swetasr1509@gmail.com>
2021-06-12 13:01:52 +0530

    Entity added

--- HousePricePredictor/Entity/HouseDetails.cs
+++ HousePricePredictor/Entity/HouseDetails.cs
@@ -8,6 +8,8 @@ namespace Entity
 {
 	public class HouseDetails
 	{
+        #region Variables
+
         private double crim = 0.0; //per capita crime rate by town
         private double zn = 0.0; // proportion of residential land zoned for lots over 25,000 sq.ft.
         private double indus = 0.0; // proportion of non-retail business acres per town
@@ -22,6 +24,123 @@ namespace Entity
         private double b = 0.0;// 1000(Bk - 0.63)^2 where Bk is the proportion of blacks  by town
         private double lstat = 0.0;//   % lower status of the population
         private double medv = 0.0; //Median value of owner-occupied homes in $1000's
+
+        #endregion
+
+        #region Properties
+
+        public double Crim
+        {
+            get { return crim; }
+            set { crim = value; }
+        }
+
+        public double Zn
+        {
+            get { return zn; }
+            set { zn = value; }
+        }
+
+        public double Indus
+        {
+            get { return indus; }
+            set { indus = value; }
+        }
+
+        public double Chas
+        {
+            get { return chas; }
+            set { chas = value; }
+        }
+
+        public double Nox
+        {
+            get { return nox; }
+            set { nox = value; }
+        }
+
+        public double Rm
+        {
+            get { return rm; }
+            set { rm = value; }
+        }
+
+        public double Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public double Dis
+        {
+            get { return dis; }
+            set { dis = value; }
+        }
+
+        public double Rad
+		{
+            get { return rad; }
+			set { rad = value; }
+		}
+
+        public double Tax
+        {
+            get { return tax; }
+            set { tax = value; }
+        }
+
+        public double Ptratio
+        {
+            get { return ptratio; }
+            set { ptratio = value; }
+        }
+
+        public double Lstat
+        {
+            get { return lstat; }
+            set { lstat = value; }
+        }
+
+        public double B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+
+        public double Medv
+        {
+            get { return medv; }
+            set { medv = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public HouseDetails()
+        {
+        }
+
+        public HouseDetails(double crim, double zn, double indus, double chas, double nox, double rm, double age, double dis,
+                            double rad, double tax, double ptratio, double b, double lstat, double medv)
+        {
+            this.crim = crim;
+            this.zn = zn;
+            this.indus = indus;
+            this.chas = chas;
+            this.nox = nox;
+            this.rm = rm;
+            this.age = age;
+            this.dis = dis;
+            this.rad = rad;
+            this.tax = tax;
+            this.ptratio = ptratio;
+            this.b = b;
+            this.lstat = lstat;
+            this.medv = medv;
+        }
+
+        #endregion
 
     }
 }
